c079603cfea411ab7bf7b84865b8db7a50a57919
moldaw <samuel.repo@gmail.com>
2024-11-12 22:10:55 +0200

    lisätty valikot etusivulle
    etusivulle kaksi vetovalikkoa hierottavalle alueelle ja ajalle

--- app/src/main/java/com/example/smartball/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/smartball/ui/home/HomeFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.smartball.R;
 import com.example.smartball.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -28,6 +32,30 @@ public class HomeFragment extends Fragment {
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Spinner areaSpinner = view.findViewById(R.id.area_spinner);
+        ArrayAdapter<CharSequence> area_adapter = ArrayAdapter.createFromResource(
+                getContext(),
+                R.array.area_options,
+                android.R.layout.simple_spinner_item
+        );
+        area_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        areaSpinner.setAdapter(area_adapter);
+
+        Spinner timeSpinner = view.findViewById(R.id.time_spinner);
+        ArrayAdapter<CharSequence> time_adapter = ArrayAdapter.createFromResource(
+                getContext(),
+                R.array.time_options,
+                android.R.layout.simple_spinner_item
+        );
+        time_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeSpinner.setAdapter(time_adapter);
+    }
+
 
     @Override
     public void onDestroyView() {
